8696bb781b84fc690585d414bdac395271931fe7
cjbruin23 <colinjb23@gmail.com>
2024-09-09 13:28:06 -0700

    cleanup

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import { Kysely, PostgresDialect } from "kysely";
 import { Pool } from "pg";
 import { Database } from "./src/types";
-import fs from 'fs';
+import fs from "fs";
 
 var cors = require("cors");
 
@@ -11,7 +11,7 @@ var cors = require("cors");
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const PORT = process.env.PORT;
 
@@ -24,9 +24,9 @@ try {
       new Pool({
         connectionString: process.env.DATABASE_URL,
         ssl: {
-          ca: fs.readFileSync('./ca.crt').toString(),
+          ca: fs.readFileSync("./ca.crt").toString(),
         },
-      })
+      }),
   });
 
   db = new Kysely<Database>({
@@ -41,21 +41,28 @@ app.get("/", (_request: Request, response: Response) => {
 });
 
 app.post("/story", async (_request: Request, response: Response) => {
-  
   const body = _request.body;
   try {
-    const storyExists = await db.selectFrom('stories').where('name', '=', body.name).executeTakeFirst();
-    if (storyExists) {
-      response.status(409).send('Book title already exists')
+    const storyExists = await db
+      .selectFrom("stories")
+      .where("name", "=", body.name)
+      .executeTakeFirst();
+    
+      if (storyExists) {
+      response.status(409).send("Book title already exists");
     } else {
-      const result = await db.insertInto('stories').values(_request.body).executeTakeFirst();
-      console.log('result', result)
+      const result = await db
+        .insertInto("stories")
+        .values(_request.body)
+        .executeTakeFirst();
       response.status(201).send("hitting story endpoint");
     }
-
+  
   } catch (err) {
-    console.error(err)
-    response.status(500).send('Error creating record. Check server for more details')
+    console.error(err);
+    response
+      .status(500)
+      .send("Error creating record. Check server for more details");
   }
 });
 
